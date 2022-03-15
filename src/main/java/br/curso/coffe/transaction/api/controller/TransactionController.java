a91ef933121dac51f1adaa93bd6b291ce474b503
Brendo Silva <breendo.01@gmail.com>
2022-03-15 09:16:52 -0300

    criação de rotas

--- src/main/java/br/curso/coffe/transaction/api/controller/TransactionController.java
+++ src/main/java/br/curso/coffe/transaction/api/controller/TransactionController.java
@@ -2,16 +2,31 @@ package br.curso.coffe.transaction.api.controller;
 
 import br.curso.coffe.transaction.api.dto.RequestTransactionDto;
 import br.curso.coffe.transaction.api.dto.TransactionDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
 
-    public Mono<TransactionDto> sendTransaction(final RequestTransactionDto requestTransactionDto){
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<TransactionDto> sendTransaction(@RequestBody final RequestTransactionDto requestTransactionDto){
+        return Mono.empty();
+    }
 
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<TransactionDto> returnTranscation(@PathVariable("id") final String uuid) {
+        return Mono.empty();
+    }
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<TransactionDto> deleteTranscation(@PathVariable("id") final String uuid) {
+        return Mono.empty();
+    }
+
+    @PatchMapping(value = "/{id}/confirmar")
+    public Mono<TransactionDto> confirmTransaction(@PathVariable("id") final String uuid){
+        return Mono.empty();
     }
 
 }
--- src/main/java/br/curso/coffe/transaction/api/dto/TransactionDto.java
+++ src/main/java/br/curso/coffe/transaction/api/dto/TransactionDto.java
@@ -1,4 +1,16 @@
 package br.curso.coffe.transaction.api.dto;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "uuid")
 public class TransactionDto {
+
+    private UUID uuid;
+
 }
