63a590f2739c2c4a9769ae1bb261bf1fe88cf5e7
Brendo Silva <breendo.01@gmail.com>
2022-08-17 19:28:22 -0300

    retry class configuration

--- src/main/java/br/curso/coffe/transaction/service/TransactionService.java
+++ src/main/java/br/curso/coffe/transaction/service/TransactionService.java
@@ -8,7 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.QueryTimeoutException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.RetryConfiguration;
 import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,23 +23,28 @@ import java.util.Optional;
 public class TransactionService {
 
     private final TransactionalRedisRepository transactionalRedisRepository;
+    private final RetryTemplate retryTemplate;
 
-
-    public TransactionService(TransactionalRedisRepository transactionalRedisRepository){
+    public TransactionService(TransactionalRedisRepository transactionalRedisRepository, RetryTemplate retryTemplate){
         this.transactionalRedisRepository = transactionalRedisRepository;
+        this.retryTemplate = retryTemplate;
     }
 
     @Transactional
+    @Retryable(value = QueryTimeoutException.class, maxAttempts = 5,backoff = @Backoff(delay = 100))
     public Optional<TransactionDto> save(final RequestTransactionDto requestTransactionDto) {
 
         requestTransactionDto.setData(LocalDateTime.now());
         return Optional.of(transactionalRedisRepository.save(requestTransactionDto));
     }
 
-    @Retryable(value = QueryTimeoutException.class, maxAttempts = 5,backoff = @Backoff(delay = 100))
+
     public Optional<TransactionDto> findById(final String id) {
-        log.info("Consultando redis");
-        return transactionalRedisRepository.findById(id);
+        return retryTemplate.execute(ret -> {
+            log.info("Consultando Redis");
+            return transactionalRedisRepository.findById(id);
+        });
+
     }
 
 
